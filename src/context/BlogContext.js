fe6a6061e6b3d1378b3d822763b447a4e80fa793
fjerbi <firasjerbiv2@gmail.com>
2021-10-27 18:16:02 +0100

    Setting up the project and create functionality

---
+++ src/context/BlogContext.js
@@ -0,0 +1,20 @@
+import React, { useReducer, useState } from "react";
+import createDataContext from "./createDataContext";
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = () => {
+  dispatch({ type: "add_blogpost" });
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, {
+  addBlogPost,
+  
+},[]);
